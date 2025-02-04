2755c6aa589588708425d2c8abfaec57382349e6
Said-Barokah <yanginiemailnyasaid@gmail.com>
2025-02-04 14:51:17 +0700

    Upload Project

---
+++ assets/js/leaflet.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([-6.257079003711817, 106.85207940434555], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([-6.257079003711817, 106.85207940434555]).addTo(map);
---
+++ assets/js/slider.js
@@ -0,0 +1,12 @@
+function toggleNext() {
+    var element = document.getElementById('leftandin1');
+    element.classList.toggle('delay-06s');
+    element.classList.toggle('fadeout');
+    element.classList.toggle('displaynone');
+    var element = document.getElementById('leftandin2');
+    element.classList.toggle('display');
+    element.classList.toggle('animated');
+    var element = document.getElementById('inner2');
+    element.classList.toggle('delay-06s');
+    element.classList.toggle('fadeIn');
+}
