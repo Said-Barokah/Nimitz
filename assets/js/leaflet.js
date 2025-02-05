89917f88f49853117fb755dfb8ab6a06b39de4f2
Said-Barokah <yanginiemailnyasaid@gmail.com>
2025-02-05 16:29:46 +0700

    Update : Marker

--- assets/js/leaflet.js
+++ assets/js/leaflet.js
@@ -1,6 +1,8 @@
 var map = L.map('map').setView([-6.257079003711817, 106.85207940434555], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 10,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 var marker = L.marker([-6.257079003711817, 106.85207940434555]).addTo(map);
+var marker = L.marker([-6.283326565455003, 106.91141267083466]).addTo(map);
+var marker = L.marker([-6.230640081832268, 106.52046743394148]).addTo(map);
