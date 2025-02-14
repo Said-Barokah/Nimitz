1ee35ea48f7701416a5c67c8211917bb8a64720f
Said-Barokah <yanginiemailnyasaid@gmail.com>
2025-02-14 12:05:00 +0700

    Update : Src JS Detail

--- assets/js/detail_product.js
+++ assets/js/detail_product.js
@@ -8,31 +8,30 @@ const params = new URLSearchParams(window.location.search);
 const nama = params.get("product");
 if(product='ball'){
     document.getElementById("mainImage").setAttribute("src", "assets/images/product/ball/ball-1.webp");
-    document.getElementById("mainImage").setAttribute("src", 'assets/images/product/ball/ball-1.webp');
-    document.getElementById("mainImage").setAttribute("src", 'assets/images/product/ball/ball-1-1.webp');
-    document.getElementById("mainImage").setAttribute("src", 'assets/images/product/ball/ball-1-2.webp');
-    document.getElementById("mainImage").setAttribute("src",  'assets/images/product/ball/ball-1-3.webp');
-    document.getElementById("mainImage").setAttribute("src",  'assets/images/product/ball/ball-1.webp');
+    document.getElementById("thumbnail-1").setAttribute("src", 'assets/images/product/ball/ball-1.webp');
+    document.getElementById("thumbnail-2").setAttribute("src", 'assets/images/product/ball/ball-1-1.webp');
+    document.getElementById("thumbnail-3").setAttribute("src", 'assets/images/product/ball/ball-1-2.webp');
+    document.getElementById("thumbnail-4").setAttribute("src",  'assets/images/product/ball/ball-1-3.webp');
 }
 if(product='cue'){
     document.getElementById("mainImage").setAttribute("src", 'assets/images/product/cue/cue-1.webp');
-    document.getElementById("mainImage").setAttribute("src", 'assets/images/product/cue/cue-1-1.webp');
-    document.getElementById("mainImage").setAttribute("src", 'assets/images/product/cue/cue-1-2.webp');
-    document.getElementById("mainImage").setAttribute("src",  'assets/images/product/cue/cue-1-3.webp');
-    document.getElementById("mainImage").setAttribute("src",  'assets/images/product/cue/cue-1.webp');
+    document.getElementById("thumbnail-1").setAttribute("src", 'assets/images/product/cue/cue-1-1.webp');
+    document.getElementById("thumbnail-2").setAttribute("src", 'assets/images/product/cue/cue-1-2.webp');
+    document.getElementById("thumbnail-3").setAttribute("src",  'assets/images/product/cue/cue-1-3.webp');
+    document.getElementById("thumbnail-4").setAttribute("src",  'assets/images/product/cue/cue-1.webp');
 }
 if(product='table'){
     document.getElementById("mainImage").setAttribute("src", 'assets/images/product/tables/table-1.webp');
-    document.getElementById("mainImage").setAttribute("src", 'assets/images/product/tables/table-1-1.webp');
-    document.getElementById("mainImage").setAttribute("src", 'assets/images/product/tables/table-1-2.webp');
-    document.getElementById("mainImage").setAttribute("src",  'assets/images/product/tables/table-1-3.webp');
-    document.getElementById("mainImage").setAttribute("src",  'assets/images/product/tables/table-1.webp');
+    document.getElementById("thumbnail-1").setAttribute("src", 'assets/images/product/tables/table-1-1.webp');
+    document.getElementById("thumbnail-2").setAttribute("src", 'assets/images/product/tables/table-1-2.webp');
+    document.getElementById("thumbnail-3").setAttribute("src",  'assets/images/product/tables/table-1-3.webp');
+    document.getElementById("thumbnail-4").setAttribute("src",  'assets/images/product/tables/table-1.webp');
 }
 if(product='lamp'){
     document.getElementById("mainImage").setAttribute("src", 'assets/images/product/lamp/lamp-1.webp');
-    document.getElementById("mainImage").setAttribute("src", 'assets/images/product/lamp/lamp-1-1.webp');
-    document.getElementById("mainImage").setAttribute("src", 'assets/images/product/lamp/lamp-1-2.webp');
-    document.getElementById("mainImage").setAttribute("src",  'assets/images/product/lamp/lamp-1-3.webp');
-    document.getElementById("mainImage").setAttribute("src",  'assets/images/product/lamp/lamp-1.webp');
+    document.getElementById("thumbnail-1").setAttribute("src", 'assets/images/product/lamp/lamp-1-1.webp');
+    document.getElementById("thumbnail-2").setAttribute("src", 'assets/images/product/lamp/lamp-1-2.webp');
+    document.getElementById("thumbnail-3").setAttribute("src",  'assets/images/product/lamp/lamp-1-3.webp');
+    document.getElementById("thumbnail-4").setAttribute("src",  'assets/images/product/lamp/lamp-1.webp');
 }
 
